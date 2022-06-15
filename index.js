b678ea9aeb82ffd227c148acded1be32b076a817
akkraz <919159845@qq.com>
2022-06-15 15:03:21 +0800

    feature: compose middlewares

--- index.js
+++ index.js
@@ -2,16 +2,23 @@ const http = require('http')
 const context = require('./lib/context')
 const request = require('./lib/request')
 const response = require('./lib/response')
+const compose = require('./lib/compose')
 
 class Koa {
+  constructor() {
+    this.middlewares = []
+  }
+
   use(callback) {
-    this.callback = callback
+    this.middlewares.push(callback)
   }
 
   listen(...args) {
     const server = http.createServer(async (req, res) => {
       const ctx = this.createContext(req, res)
-      await this.callback(ctx)
+      const action = compose(this.middlewares)
+      await action(ctx)
+
       res.end(ctx.body)
     })
 
